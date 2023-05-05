eed79155041b7b19e9a85f7dd607eb2930f61914
Dmitry Shutov <dmitry.s@beda.software>
2023-05-05 12:21:00 +0700

    Update ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,51 @@
 module.exports = {
-    env: {
-        'jest/globals': true,
-        browser: true,
-    },
-    root: true,
-    extends: ['prettier'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', 'jest', 'import'],
-    globals: {
-        JSX: true,
-        node: true,
-        es6: true,
-        jest: true,
-    },
-    rules: {
-        'react-native/no-inline-styles': 0,
-        'prettier/prettier': 0,
-        'import/order': [
-            'error',
-            {
-                groups: ['builtin', 'external', 'internal', ['index', 'sibling', 'parent']],
-                'newlines-between': 'always',
-                pathGroupsExcludedImportTypes: ['builtin'],
-                pathGroups: [
-                    {
-                        pattern: 'shared/**',
-                        group: 'internal',
-                        position: 'after',
-                    },
-                    {
-                        pattern: 'src/**',
-                        group: 'internal',
-                        position: 'after',
-                    },
-                ],
-                alphabetize: { order: 'asc', caseInsensitive: true },
-            },
+  env: {
+    'jest/globals': true,
+    browser: true,
+  },
+  root: true,
+  extends: ['prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'jest', 'import'],
+  globals: {
+    JSX: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  rules: {
+    'react-native/no-inline-styles': 0,
+    'prettier/prettier': 0,
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', ['index', 'sibling', 'parent']],
+        'newlines-between': 'always',
+        pathGroupsExcludedImportTypes: ['builtin'],
+        pathGroups: [
+          {
+            pattern: 'aidbox-react/**',
+            group: 'external',
+            position: 'after',
+          },
+          {
+            pattern: 'fhir-react/**',
+            group: 'external',
+            position: 'after',
+          },
+          {
+            pattern: 'shared/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: 'src/**',
+            group: 'internal',
+            position: 'after',
+          },
         ],
-    },
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
+  },
 };
